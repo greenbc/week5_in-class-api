08f10e8cec211f76a5fe56c301ea5a96bd151aed
Ben Green <greenbc005@gmail.com>
2021-02-02 18:29:13 -0500

    2nd day upload

--- drone_api/__init__.py
+++ drone_api/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
 
-# TODO: Import config objeft for Flask project
+# TODO: Import config object for Flask project
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from drone_api import routes, models
