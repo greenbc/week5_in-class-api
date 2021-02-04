295d39113dbf91c22e03439b7e10c931e304aebd
Ben Green <greenbc005@gmail.com>
2021-02-03 21:31:08 -0500

    3rd day upload

--- drone_api/__init__.py
+++ drone_api/__init__.py
@@ -5,10 +5,20 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# Import for Flask Login
+from flask_login import LoginManager
+
+# Import for AuthLib integrations
+from authlib.integrations.flask_client import OAuth
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin' # Specify what page to load for NON-AUTHED users
+
+oauth = OAuth(app)
 
 from drone_api import routes, models
--- drone_api/models.py
+++ drone_api/models.py
@@ -1,11 +1,23 @@
-from drone_api import app, db
+from drone_api import app, db, login_manager
 import uuid
 from datetime import datetime
 
 # Adding Flask Security for Passwords
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+# Import for Secrets Module (Given by Python)
+import secrets
+
+# Imports for Login Manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# load_user = user_loader(load_user)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True)
     first_name = db.Column(db.String(150), nullable = True, default='')
     last_name = db.Column(db.String(150), nullable = True, default='')
@@ -14,13 +26,19 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default='')
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    drone = db.relationship('Drone', backref = 'owner', lazy = True)
 
-    def __init__(self,email,first_name = '', last_name= '', id = '', password = ''):
+    def __init__(self,email,first_name = '', last_name= '', id = '', password = '', token = '', g_auth_verify = False):
         self.id = self.set_id()
         self.first_name = first_name
         self.last_name = last_name
         self.password = self.set_password(password)
         self.email = email
+        self.token = self.set_token(24)
+        self.g_auth_verify = g_auth_verify
+
+    def set_token(self, length):
+        return secrets.token_hex(length)
 
     def set_id(self):
         return str(uuid.uuid4())
@@ -31,3 +49,27 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been added to the database'
+
+class Drone(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(150))
+    price = db.Column(db.Integer)
+    model = db.Column(db.String(150))
+    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable = False)
+
+    def __init__(self,name,price,model,user_id):
+        self.name = name
+        self.price = price
+        self.model = model
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'The following Drone has been added: {self.name} which belongs to {self.user_id}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "model": self.model
+        }
